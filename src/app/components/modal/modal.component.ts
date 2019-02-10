780cd022e5026ea2c8e8fee6c64789771b244f6f
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2019-02-09 23:22:03 -0500

    Imagenes x macota

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 import { DataApiService } from '../../services/data-api.service';
 import { NgForm } from '@angular/forms';
 import { MascotInterface } from './../../models/mascot';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-modal',
@@ -10,10 +13,23 @@ import { MascotInterface } from './../../models/mascot';
 })
 export class ModalComponent implements OnInit {
 
-  constructor(private dataApi: DataApiService) { }
+  constructor(private dataApi: DataApiService, private storage: AngularFireStorage) { }
   @ViewChild('btnClose') btnClose: ElementRef;
   @Input() userUid: string;
   ngOnInit() {
+  }
+  uploadPercent: Observable<number>;
+  urlImage: Observable<string>;
+
+  onUpload(e) {
+    // console.log('subir', e.target.files[0]);
+    const id = Math.random().toString(36).substring(2);
+    const file = e.target.files[0];
+    const filePath = `mascots/mascot_${id}`;
+    const ref = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+    this.uploadPercent = task.percentageChanges();
+    task.snapshotChanges().pipe(finalize(() => this.urlImage = ref.getDownloadURL())).subscribe();
   }
 
   onSaveMascot(formMascot: NgForm): void {
